9274cacd013d4fa774e651c5a2260c02fe386ffd
Jin Oh <freenozero@naver.com>
2023-01-04 15:24:05 +0900

    json 파일명을 기준으로 aug로 변경해야하고, check_seg 작성 완료

---
+++ 230103/check_seg.py
@@ -0,0 +1,24 @@
+import json
+import cv2
+import numpy as np
+
+json_path = 'D:/wp/data/xray_artknife_a_1/json/crop_data.json'
+with open(json_path) as json_file:
+    json_data = json.load(json_file)
+
+for i in range(200, 220):
+    img = cv2.imread(json_data['images'][i]['path'], -1)
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
+    print(i)
+    seg = []
+    for j in range(0, len(json_data['annotations'][i]['segmentation'][0][0])):
+        if j % 2 == 0:
+            seg.append(([json_data['annotations'][i]['segmentation'][0][0][j], json_data['annotations'][i]['segmentation'][0][0][j+1]]))
+
+    seg = np.array(seg, np.int32)
+    img_seg = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    cv2.fillConvexPoly(img, seg, color=(255,0,0))
+   
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
--- 230103/data augmentation.py
+++ 230103/data augmentation.py
@@ -9,23 +9,26 @@ def augmentation(file_path, json_path):
     file = os.listdir(file_path)
     file = natsort.natsorted(file) # 정렬 (파일 불러오면 1, 10, 11 ... list 저장 해결법)
     data_max = len(file) - 1  # -1은 Thumbs.db 제외
-    print(data_max)
+    # print(data_max)
 
-    # json 파일 불러오기
-    with open(json_path) as f:
-        json_data = json.load(f)
 
     # 한 세트(row, high당 3번 업데이트)
-    for _ in range(0, 3):
+    for _ in range(0, 1):
         # 파일 하나씩 불러오기
         for f in file:
-            file_name = int(f.rstrip('.png'))
+            # json 파일 불러오기
+            with open(json_path) as json_file:
+                json_data = json.load(json_file)
+
             # Thumbs.db는 제외
             if (f != 'Thumbs.db'):
+                
+                file_name = int(f.rstrip('.png'))
                 # file 이름이 짝수일때마다 random 생성
                 
                 #-1은 16 bit 이미지 불러오기 위함
                 img = cv2.imread(file_path + '/' + f, -1)
+                print(f)
 
                 #update_fx와 값을 다르게 주기 위해 if문 두개
                 if (file_name % 2 == 0):
@@ -35,26 +38,31 @@ def augmentation(file_path, json_path):
 
                 # 상대적인 비율로 축소, 확대
                 update_img = cv2.resize(img, dsize=(0, 0), fx=update_cols, fy=update_rows, interpolation=cv2.INTER_LINEAR)
-                print('원본(height, width):', img.shape)
-                print('업데이트(height, width):', update_img.shape)
-                print('fx, fy:', update_cols, update_rows)
+                # print('원본(height, width):', img.shape)
+                # print('업데이트(height, width):', update_img.shape)
+                # print('fx, fy:', update_cols, update_rows)
                 
-                cv2.imshow('img', img)
-                cv2.imshow('update_img', update_img)
-                cv2.waitKey(0)
+             
 
                 #segmentation update: seg(x) * update_rows, seg(y) * update_rows
                 new_seg = list()
+                # print(file_name)
+                # print(json_data)
+                # print(json_data['annotations'][file_name]['segmentation'])
+                num = 0
+                # print(update_rows)
+                # print(update_cols)
                 for i in json_data['annotations'][file_name]['segmentation'][0][0]:
-                    num = 0
+                    # print(i)
                     if((num%2) == 0):
-                        new_seg.append(round(i * update_rows,1))
+                        new_seg.append(round(i * update_rows, 1))
                     else:
                         new_seg.append(round(i * update_cols, 1))
+                    # print(new_seg)
                     num += 1
                 
-                print(json_data['annotations'][file_name]['segmentation'])
-                print(new_seg)
+                # print(json_data['annotations'][file_name]['segmentation'])
+                # print(new_seg)
                 
                # json_data['annotations'][file_name]['segmentation'][0][0] = new_seg
 
@@ -63,7 +71,7 @@ def augmentation(file_path, json_path):
                                       'image_id': data_max,
                                       'category_id': json_data['annotations'][file_name]['category_id'],
                                       'bbox': [0, 0, update_img.shape[1], update_img.shape[0]], #x, y, width, height
-                                      'segmentation': [[[new_seg]]],
+                                      'segmentation': [[new_seg]],
                                       'area': update_img.shape[0]*update_img.shape[1], #height * width
                                       'iscrowd': False,
                                       'color': 'Unknown',
@@ -82,13 +90,17 @@ def augmentation(file_path, json_path):
                                   'height': update_img.shape[0]}
                 json_data['images'].append(new_images)
 
-                print(new_annotations)
-                print(new_images)
+                #이미지 보기
+                # cv2.imshow('img', img) #이전
+                # cv2.imshow('update_img', update_img)
+                # cv2.waitKey(0)
+
+                # print(new_annotations)
+                # print(new_images)
 
                 #json 파일 저장하기 
-                print(json_path)
-                with open(json_path, 'w') as f:
-                    json_data = json.dump(json_data, f)
+                with open(json_path, 'w') as json_file:
+                    json_data = json.dump(json_data, json_file)
 
                 #이미지 파일 저장하기
                 cv2.imwrite(file_path + '/' + str(data_max) + '.png', update_img)
@@ -99,3 +111,4 @@ def augmentation(file_path, json_path):
 file_path = 'D:/wp/data/xray_artknife_a_1/crop'
 json_path = 'D:/wp/data/xray_artknife_a_1/json/crop_data.json'
 augmentation(file_path, json_path)
+
