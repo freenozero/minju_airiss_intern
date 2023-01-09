ab94d728f8b0f72870ec8c1940ba9aa6744ad6af
Jin Oh <freenozero@naver.com>
2023-01-09 12:37:29 +0900

    ⚡️bbox, seg 원본에 그리기

--- data_augmentation/data_augmentation.py
+++ data_augmentation/data_augmentation.py
@@ -5,53 +5,43 @@ import numpy as np
 import random
 import natsort
 
-
-def augmentation(file_path, json_path, img_name):
-    file = os.listdir(file_path)
-    if "Thumbs.db" in file:
-        file.remove("Thumbs.db")
-    file = natsort.natsorted(file)  # 정렬 (파일 불러오면 1, 10, 11 ... list 저장 해결법)
-
+def augmentation(img_name):
+    file_path = f"D:/wp/data/{img_name}/crop"
+    json_path = f"D:/wp/data/{img_name}/json/crop_data.json"
+    
+    file = [f for f in os.listdir(file_path) if f.endswith('.png')] # png 파일만 불러오기
+    file = natsort.natsorted(file)  # 정렬
+    
     # json 파일 불러오기
     with open(json_path) as json_file:
         json_data = json.load(json_file)
 
+    # json 파일의 file_name = i 형식으로 불러오기
     json_file_dic = {}
     for i in range(len(json_data['images'])):
         file_name = json_data['images'][i]['file_name']
         json_file_dic[file_name] = i
-
-    save_file_name = file[len(file)-1].rstrip('.png')
     
-    save_file_name = str(int(save_file_name)+ 1) 
+    # 저장할 파일 이름을 crop 폴더 max에서 +1
+    save_file_name = file[len(file)-1].rstrip('.png')
+    #시작 파일 짝수 홀수
+    first_file_name = int(file[0].rstrip('.png')) % 2
+
+    # 한 장에 몇 번 저장할지
     for _ in range(0, 1):
+        # 파일 하나씩 불러오기
         for f in file:
-            # json 파일 불러오기
-            with open(json_path) as json_file:
-                json_data = json.load(json_file)
-            
             file_name = int(f.rstrip('.png'))
-            # print(len(json_data['images']) + diff, file_name)
 
             # -1은 16 bit 이미지 불러오기 위함
             img = cv2.imread(file_path + '/' + f, -1)
-            # file 이름이 짝수일때마다 random 생성
-            # update_fx와 값을 다르게 주기 위해 if문 두개
 
-            # 이미지 시작 파일이 짝수인지 홀수인지 구별 후 random 
-            if ((int(file[0].rstrip('.png'))%2) == 0): 
-                if ((file_name) % 2 == 0):
-                    # x update: img.shape[0] / update_img.shape[0]
+            if (first_file_name == 0 and file_name % 2 == 0): #시작 파일이 짝수고, 현재 증강할 파일이 짝수면 random
                     update_cols = round(random.uniform(0.5, 1.5), 1)
-                if ((file_name) % 2 == 0):
                     update_rows = round(random.uniform(0.5, 1.5), 1)  # y update
-            else:
-                if ((file_name) % 2 != 0):
+            elif (first_file_name != 0 and file_name % 2 != 0): #시작 파일이 홀수고, 현재 증강할 파일이 홀수면 random
                     update_cols = round(random.uniform(0.5, 1.5), 1)
-                if ((file_name) % 2 != 0):
                     update_rows = round(random.uniform(0.5, 1.5), 1)  # y update
-            
-
 
             # 상대적인 비율로 축소, 확대
             update_img = cv2.resize(img, dsize=(
@@ -60,11 +50,10 @@ def augmentation(file_path, json_path, img_name):
             # print('업데이트(height, width):', update_img.shape)
             # print('fx, fy:', update_cols, update_rows)
 
+            
             # segmentation update: seg(x) * update_rows, seg(y) * update_rows
             new_seg = list()
             num = 0
-            
-            #seg
             for i in json_data['annotations'][json_file_dic[f]]['segmentation'][0][0]:
                 if ((num % 2) == 0):
                     new_seg.append(round(i * update_cols, 1))
@@ -72,9 +61,11 @@ def augmentation(file_path, json_path, img_name):
                     new_seg.append(round(i * update_rows, 1))
                 # print(new_seg)
                 num += 1
-
+            
+            # 파일이 가위일 경우
+            save_file_name = str(int(save_file_name)+ 1) 
             if "xray_scissors" in img_name:
-                save_file_name.zfill(5)
+                save_file_name = save_file_name.zfill(5)
 
             # json 파일 추가
             new_images = {'id': len(json_data['images']) +1,
@@ -124,26 +115,24 @@ def augmentation(file_path, json_path, img_name):
 
             # 이미지 파일 저장
             cv2.imwrite(file_path + '/' + save_file_name + '.png', update_img)
-
-            # json 파일 저장
-            with open(json_path, 'w') as json_file:
-                json_data = json.dump(json_data, json_file)
-            
             file_name += 1
-            save_file_name = str(int(save_file_name)+ 1) 
-
-            if "xray_scissors" in img_name:
-                save_file_name = save_file_name.zfill(5)
+    
+    # json 파일 저장
+    with open(json_path, 'w') as json_file:
+        json_data = json.dump(json_data, json_file)
             
 
+def filter_image(img_name):
+    file_path = f"D:/wp/data/{img_name}/crop"
+    json_path = f"D:/wp/data/{img_name}/json/crop_data.json"
+    origin_img_path = f"D:/wp/data/원본/{img_name}/crop"
+    add_img_path = f"D:/wp/data/{img_name}/add_image"
 
-def filter_image(json_path, origin_img_path, file_path, add_img_path):
-    origin_file = os.listdir(origin_img_path)
-    # 정렬 (파일 불러오면 1, 10, 11 ... list 저장 해결법)
+    origin_file = [f for f in os.listdir(origin_img_path) if f.endswith('.png')]
     origin_file = natsort.natsorted(origin_file)
 
-    file = os.listdir(file_path)
-    file = natsort.natsorted(file)  # 정렬 (파일 불러오면 1, 10, 11 ... list 저장 해결법)
+    file = [f for f in os.listdir(file_path) if f.endswith('.png')]
+    file = natsort.natsorted(file)
 
    # json 파일 불러오기
     with open(json_path) as json_file:
@@ -159,8 +148,9 @@ def filter_image(json_path, origin_img_path, file_path, add_img_path):
     file = [x for x in file if x not in s]
     
     for i in file:
-        img = cv2.imread(file_path + '/' + i, 1)
-
+        original_img = cv2.imread(file_path + '/' + i, 1)
+        filter_img = original_img.copy()
+        
         # segmentation 불러오기
         seg = []
         file_name = str(i.rstrip('.png'))
@@ -174,17 +164,15 @@ def filter_image(json_path, origin_img_path, file_path, add_img_path):
         bbox = json_data['annotations'][json_file_dic[i]]['bbox']
 
         # seg 칠하기
-        filter_img = np.full(
-            (img.shape[0], img.shape[1], 3), (0, 0, 0), dtype=np.uint8)
-        cv2.fillConvexPoly(filter_img, seg, color=(0, 0, 255, 1))
+        cv2.fillPoly(filter_img, [seg], (0, 0, 255))
 
         # bbox 그리기
         cv2.rectangle(filter_img, (bbox[0], bbox[1]),
                       (bbox[2], bbox[3]), (255, 0, 0, 1), 3)
 
-        # img랑 seg_img 합성하기
-        add_img = cv2.addWeighted(img, 0.7, filter_img, 0.3, 0)
-
+        # original_img랑 filter_img 합성하기
+        add_img = cv2.addWeighted(original_img, 0.7, filter_img, 0.3, 3)
+        
         # 합성한 이미지 보기
         # cv2.imshow("add_img", add_img)
         # cv2.waitKey(0)
@@ -197,10 +185,5 @@ def filter_image(json_path, origin_img_path, file_path, add_img_path):
 # img_name만 변경하면 됨.
 img_name = "xray_scissors_1"
 
-file_path = f"D:/wp/data/{img_name}/crop"
-json_path = f"D:/wp/data/{img_name}/json/crop_data.json"
-origin_img_path = f"D:/wp/data/원본/{img_name}/crop"
-add_img_path = f"D:/wp/data/{img_name}/add_image"
-
-augmentation(file_path, json_path, img_name)
-filter_image(json_path, origin_img_path, file_path, add_img_path)
+augmentation(img_name)
+filter_image(img_name)
